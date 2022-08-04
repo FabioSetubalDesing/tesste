9065e9c064d9692d73d00abb1afe74e14cc1ce34
Fabio Setubal <fabiodesenvolvedorpro@gmail.com>
2022-08-03 21:16:49 -0300

    3.2 versao

---
+++ cadastro.php
@@ -0,0 +1,46 @@
+<!--Sintaxe PHP-->
+<style>
+    h1{
+        text-align: center;
+    }
+    a{
+        text-decoration: none;
+        background-color: green;
+        color:white;
+        border-radius: 5px;
+        padding: 5px;
+    }
+
+    a:hover{
+        opacity: 0.5;
+    }
+</style>
+<?php
+    //incluindo conexão.php
+    include("conexao.php");
+    
+    //criando variaveis
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $texto = $_POST['message'];
+
+//é uma linha de codigo só
+//novos é o nome da tabela
+//nomes das colunas da tabela
+//em seguida nome das variaveis  criadas aqui
+$sql ="INSERT INTO cliente(`nome`, `email`, `telefone`, `pedido`)
+ VALUES ('$nome','$email','$telefone','$texto')";
+
+//para verificar se usuario foi cadastrado
+    if(mysqli_query($conexao, $sql)){
+        echo "<h1>Pedido enviado com sucesso!</h1>";
+        echo" <h1><a href='tela.html'>Voltar</a></h1>";
+    }else{
+        echo "<h1>Pedido não enviado</h1>".mysqli_connect_errno($conexao);
+    }
+
+//encerra conexao  mysqli apos os cadastros, para não trava o banco de dados
+    mysqli_close($conexao);
+
+?>
